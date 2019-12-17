aaa0de0cb1a1d81bec2edc545b36cd11af6a8a2b
Amaia <amaia.berrocal@gmail.com>
2019-12-18 00:37:38 +0100

    Creación de base html, procesamiento de datos

--- myapp/routes.py
+++ myapp/routes.py
@@ -1,9 +1,38 @@
 from myapp import app
 from flask import render_template
+import csv
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    fSales = open('../sales10.csv', 'r')
+    csvreader = csv.reader(fSales, delimiter=',')
+    registros = []
+    for linea in csvreader:
+        registros.append(linea)
+
+    cabecera = registros[0]
+    ventas = []
+    for datos in registros[1:]:
+        d = {}
+        for ix, nombre_campo in enumerate(cabecera): #delego todo el control en python
+            d[nombre_campo] = datos[ix]    
+        ventas.append(d)
+        '''
+        otras formas de hacer lo mismo
+        i = 0
+        for nombre_campo in cabecera: #delego el control del nombre y yo llevo el indice
+            d[nombre_campo] = datos[i]
+            i += 1
+        
+        for ix in range (len(cabecera)): #delego el control del indice y yo llevo el nombre
+            nombre_campo = cabecera[ix]
+            d[nombre_campo]= datos [ix]
+        '''   
+        resultado = [] #lista de diccionarios
+        '''
+        recorrer el diccionario creando uno nuevo con region, ingresos totales y beneficios totales
+        '''
+    return render_template('index.html', registros=resultados)
 
 @app.route("/detail")
 def detail():
