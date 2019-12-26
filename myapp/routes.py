15f4f2b7a1e340c87953a0d15a387502992b6467
Amaia <amaia.berrocal@gmail.com>
2019-12-26 11:49:56 +0100

    Página detail Sin Pais corregida

--- myapp/routes.py
+++ myapp/routes.py
@@ -10,22 +10,22 @@ for linea in csvreader: #recorro cada línea de csvreader, separando sus compone
     registros.append(linea) #añado cada lista (cada línea) a resgistros
 
 cabecera = registros[0]
-ventas = []
+ventas = [] #aquí tenemos cada uno de los registros en forma de diccionario(9 diccionarios)
 for datos in registros[1:]:
     d = {}
     for ix, nombre_campo in enumerate(cabecera):
         d[nombre_campo] = datos[ix]    
-    ventas.append(d)
+    ventas.append(d) #meteremos cada uno de los registros en forma de diccionario(9 diccionarios) en ventas
 
 @app.route('/')
 def index():  
-    datos = {}
-    for linea in ventas:
-        if linea['region'] in datos:
+    datos = {} #creo un diccionario vacio (datos)
+    for linea in ventas: #recorro ventas linea por linea y miro la región
+        if linea['region'] in datos: #si está la región (ej.Australia) en datos, la recupero y le sumo los ingresos y beneficios
             regAct = datos[linea['region']]
             regAct['ingresos_totales'] += float(linea['ingresos_totales'])
             regAct['beneficios_totales'] += float(linea['beneficio'])
-        else:
+        else: #si no está, la creo en datos{} y le añado los ingresos y beneficios
             datos[linea['region']] = {'ingresos_totales': float(linea['ingresos_totales']), 'beneficios_totales': float(linea['beneficio'])}
     
     resultado = [] #recorro datos, obtengo la clave y creo una tupla con clave-valor. Resultados es una lista de tuplas.
@@ -35,11 +35,11 @@ def index():
     return render_template('index.html', registros=resultado)
 
 @app.route("/detail")
-def detail():
+def detail(): #hacemos exactamente lo mismo que en index pero con 'pais'. No metemos los datos en lista para ver como se trabaja con en diccionario
     datos = {}
     region_name = request.values['region']
     for linea in ventas:
-        if linea['region'] == region_name:
+        if linea['region'] == region_name: #olo tengo que meter los paises de la region que hemos pedido
             if linea['pais'] in datos:
                 regAct = datos[linea['pais']]
                 regAct['ingresos_totales'] += float(linea['ingresos_totales'])
